29fc317e950afae5ea6803be97d031d705e2f59b
Masha <70323469+MeoWeb@users.noreply.github.com>
2021-02-20 10:05:54 +0300

    task.js updated

--- 1.2-extended-syntax/task.js
+++ 1.2-extended-syntax/task.js
@@ -1,14 +1,62 @@
-function getResult(a,b,c){
-    // код для задачи №1 писать здесь
-    // return x;
+function getResult(a, b, c) {
+    'use strict';
+
+    let result = [];
+    let D = b ** 2 - 4 * a * c;
+
+    if (D < 0)
+        return result;
+
+    if (D == 0) {
+        result = (-b + Math.sqrt(D)) / (2 * a);
+        return result;
+    }
+    else if (D > 0) {
+
+        result.push((-b + Math.sqrt(D)) / (2 * a));
+        result.push((-b - Math.sqrt(D)) / (2 * a));
+        return result;
+    }
 }
 
-function getAverageMark(marks){
-    // код для задачи №2 писать здесь
-    // return averageMark;
+
+function getAverageMark(marks) {
+    "use strict";
+
+    let marksSum = marks.reduce(function (sum, current) {
+        return sum + current;
+    }, 0);
+
+    let averageMark = marksSum / marks.length;
+
+    if (marks.length > 0 && marks.length < 6) {
+        return averageMark;
+    } else if (marks.length > 5) {
+        let lastMarks = marks.slice(-5);
+        let lastMarksSum = lastMarks.reduce(function (sum, current) {
+            return sum + current;
+        }, 0);
+        let newAverageMark = lastMarksSum / lastMarks.length;
+        console.log('Оценок больше 5-ти. Идет подсчет среднего значения последних 5-ти оценок.')
+        return newAverageMark;
+    }
+    else {
+        return 0;
+    }
+
 }
 
-function askDrink(name,dateOfBirthday){
-    // код для задачи №3 писать здесь
-    // return result;
+
+
+
+
+function askDrink(personName, dateOfBirthday) {
+
+    let newDate = dateOfBirthday.getFullYear();
+    if (newDate <= 2001) {
+        return `Не желаете ли олд-фэшн, ${personName}?`;
+    }
+    else {
+        return `Сожалею, ${personName}, но я не могу вам продать алкоголь. Могу предложить вам замечательный клюквенный компот!`
+    }
 }
