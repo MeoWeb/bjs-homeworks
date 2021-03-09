de8e97a1f181502985369467d4129fd8e3064372
Masha <70323469+MeoWeb@users.noreply.github.com>
2021-03-09 08:39:05 +0300

    task.js updated

--- 1.3-data-types/task.js
+++ 1.3-data-types/task.js
@@ -1,9 +1,35 @@
+'use strict';
+
 function calculateTotalMortgage(percent, contribution, amount, date) {
-    // код для задачи №1 писать здесь
-    // return totalAmount;
+
+    percent = Number(percent);
+    contribution = Number(contribution);
+    amount = Number(amount);
+
+    percent = percent / 100;
+
+    let S = amount - contribution;;
+    let today = new Date();
+
+    let checkDateYear = date.getFullYear() - today.getFullYear();
+    let checkDateMonth = date.getMonth() - today.getMonth();
+
+    let numberOfMonths = checkDateMonth + (checkDateYear * 12);
+    let P = percent * (1 / 12);
+
+    let monthAmount = S * (P + P / (((1 + P) ** numberOfMonths) - 1));
+    let totalAmount = monthAmount * numberOfMonths;
+
+    console.log(totalAmount);
+    return totalAmount.toFixed(2);
 }
 
 function getGreeting(name) {
-    // код для задачи №2 писать здесь
-    // return greeting;
+
+    if (name === '') {
+        return `Привет, мир! Меня зовут Аноним.`;
+    } else {
+        return `Привет, мир! Меня зовут ${name}.`;
+    }
 }
+
