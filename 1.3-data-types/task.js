0dbeb9448055e96fb4e566497aa35f22f047f9f4
Masha <70323469+MeoWeb@users.noreply.github.com>
2021-03-15 21:30:58 +0300

    task.js updated 1.2

--- 1.3-data-types/task.js
+++ 1.3-data-types/task.js
@@ -2,15 +2,22 @@
 
 function calculateTotalMortgage(percent, contribution, amount, date) {
 
-    console.log('percent ' + typeof percent);
-    console.log('contribution ' + typeof contribution);
-    console.log('amount ' + typeof amount);
 
-    Percent = Number(percent);
-    Contribution = Number(contribution);
-    Amount = Number(amount);
 
+    percent = Number(percent);
+    contribution = Number(contribution);
+    amount = Number(amount);
     percent = percent / 100;
+
+    console.log(percent);
+    console.log(contribution);
+    console.log(amount);
+
+
+    if (percent.isNan || contribution.isNan || amount.isNan ||
+        percent <= 0 || contribution < 0 || amount <= 0) {
+        return 'Ошибка';
+    }
 
     let S = amount - contribution;;
     let today = new Date();
@@ -18,16 +25,20 @@ function calculateTotalMortgage(percent, contribution, amount, date) {
     let checkDateYear = date.getFullYear() - today.getFullYear();
     let checkDateMonth = date.getMonth() - today.getMonth();
 
+    if (checkDateYear < 0) {
+        return 'Ошибка';
+    }
+
     let numberOfMonths = checkDateMonth + (checkDateYear * 12);
     let P = percent * (1 / 12);
 
     let monthAmount = S * (P + P / (((1 + P) ** numberOfMonths) - 1));
     let totalAmount = monthAmount * numberOfMonths;
 
-
     console.log(totalAmount);
-    return totalAmount.toFixed(2);
+    return Number(totalAmount.toFixed(2));
 }
+
 
 function getGreeting(name) {
     let newName;
