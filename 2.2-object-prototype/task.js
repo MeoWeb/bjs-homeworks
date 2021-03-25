06e24eb77896db1dd32ce1b817f2b4da12a5623e
Masha <70323469+MeoWeb@users.noreply.github.com>
2021-03-25 15:49:23 +0300

    task.js updated 2,3

--- 2.2-object-prototype/task.js
+++ 2.2-object-prototype/task.js
@@ -1,11 +1,27 @@
+'use sctrict';
+
 //String.prototype.isPalindrome - для задачи №1
 
 function getAverageMark(marks) {
-    // код для задачи №2 писать здесь
-    // return averageMark
+    let sum = 0;
+    for (let i = 0; i < marks.length; i++) {
+        sum = sum + +marks[i];
+    }
+    let average = sum / marks.length;
+    let averageMark = Math.round(average);
+    if (marks.length == "") {
+        return 0;
+    } else
+        return averageMark;
 }
 
+
 function checkBirthday(birthday) {
-    // код для задачи №3 писать здесь
-    // return verdict
+    let now = new Date();
+    birthday = new Date(birthday);
+    let difference = now - birthday;
+    let millisecondsYear = 24 * 60 * 60 * 1000 * 365;
+    let age = difference / millisecondsYear;
+
+    return (age > 18);
 }
